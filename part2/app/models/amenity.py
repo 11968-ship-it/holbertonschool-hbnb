fab5baadbed56f8cc7098b95d3b3a0e9928832c2
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-04 18:56:09 +0300

    Add staticmethod for amenity

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -5,3 +5,14 @@ class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    @staticmethod
+    def _validate_name(value):
+        if not isinstance(value, str):
+            raise TypeError "Name must be a string!"
+        value = value.strip()
+        if not value:
+            raise ValueError "Name is required!"
+        if len(value) > 50:
+            raise ValueError "Name must be at most 50 characters!"
+        return value
