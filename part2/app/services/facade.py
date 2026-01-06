abba4da7d861c78947b24af58831e6d1c2a483ab
11968-ship-it <11968@holbertonstudents.com>
2026-01-06 17:47:31 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,8 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -177,3 +179,29 @@ class HBnBFacade:
         # Remove from repo
         self.review_repo.delete(review_id)
         return True
+
+
+
+    # --- Amenities ---
+    def create_amenity(self, amenity_data):
+        name = amenity_data.get("name")
+        amenity = Amenity(name=name)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+
+        if "name" in amenity_data:
+            amenity.name = amenity_data["name"]
+
+        self.amenity_repo.update(amenity)
+        return amenity
