1f9f292052dd6f2d37e53f3654f4bc96148529ff
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-06 18:23:27 +0300

    Update __init__.py

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -6,7 +6,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
-    
+
 api = Api(
     version='1.0',
     title='HBnB API',
