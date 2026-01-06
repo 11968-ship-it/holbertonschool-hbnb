71e4c37136709bfafc370e1a2a4abe1559eaa947
11968-ship-it <11968@holbertonstudents.com>
2026-01-06 19:05:58 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -186,25 +186,29 @@ class HBnBFacade:
 
 
     # --- Amenities ---
-    def create_amenity(self, amenity_data):
-        name = amenity_data.get("name")
-        amenity = Amenity(name=name)
-        self.amenity_repo.add(amenity)
-        return amenity
+   # --- Amenities ---
+def create_amenity(self, amenity_data):
+    name = amenity_data.get("name")
+    amenity = Amenity(name=name)
+    self.amenity_repo.add(amenity)
+    return amenity
 
-    def get_amenity(self, amenity_id):
-        return self.amenity_repo.get(amenity_id)
+def get_amenity(self, amenity_id):
+    return self.amenity_repo.get(amenity_id)
 
-    def get_all_amenities(self):
-        return self.amenity_repo.get_all()
+def get_all_amenities(self):
+    return self.amenity_repo.get_all()
 
-    def update_amenity(self, amenity_id, amenity_data):
-        amenity = self.amenity_repo.get(amenity_id)
-        if not amenity:
-            return None
+def update_amenity(self, amenity_id, amenity_data):
+    amenity = self.amenity_repo.get(amenity_id)
+    if not amenity:
+        return None
 
-        if "name" in amenity_data:
-            amenity.name = amenity_data["name"]
+    # Update attributes on the Amenity object
+    if "name" in amenity_data:
+        amenity.name = amenity_data["name"]
 
-        self.amenity_repo.update(amenity)
-        return amenity
+    # Pass both object and data to the repository
+    self.amenity_repo.update(amenity, amenity_data)
+
+    return amenity
