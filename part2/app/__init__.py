21b33f1605acb2bc9ef476c296289b35ebd3d3b1
11968-ship-it <11968@holbertonstudents.com>
2026-01-06 18:10:53 +0300

    Update __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -18,10 +18,5 @@ def create_app():
         description='HBnB Application API',
         doc='/api/v1/'
     )
-    
-    # Register the users namespace
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
     return app
