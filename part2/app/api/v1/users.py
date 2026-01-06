a0583abe5b22e5edc09dfc816c3a93391e29ae22
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-06 17:46:44 +0300

    Update users.py

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -42,3 +42,14 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+        
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User updated successfully')
+    @api.response(404, 'User not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, user_id):
+        user_data = api.payload
+        updated_user = facade.update_user(user_id, user_data)
+        if not updated_user:
+            return {'error': 'User not found'}, 404
+        return {'message': 'User updated successfully'}, 200
