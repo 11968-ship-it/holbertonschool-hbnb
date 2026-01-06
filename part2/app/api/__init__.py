81c2de1099c9c645b0bb31aefd0dd622e2618950
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-06 18:16:35 +0300

    Update __init__.py

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -7,13 +7,12 @@ from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
     
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        doc='/api/v1/'
-    )
+api = Api(
+    version='1.0',
+    title='HBnB API',
+    description='HBnB Application API',
+    doc='/api/v1/'
+)
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
