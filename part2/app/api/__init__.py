c91c27eeb6350be34e42fc5e5f8bb0ee27f31f47
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-06 18:24:03 +0300

    Update __init__.py

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -1,19 +1,15 @@
 from flask_restx import Api
-
 # Importing namespaces
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
-
-
 api = Api(
     version='1.0',
     title='HBnB API',
     description='HBnB Application API',
     doc='/api/v1/'
 )
-
 # Register the users namespace
 api.add_namespace(users_ns, path='/api/v1/users')
 api.add_namespace(amenities_ns, path='/api/v1/amenities')
