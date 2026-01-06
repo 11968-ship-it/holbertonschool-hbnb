2e77d9264db3e12bc7178d87d2ead61c17fd783e
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-06 18:22:20 +0300

    Update __init__.py

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -14,7 +14,7 @@ api = Api(
     doc='/api/v1/'
 )
 
-    # Register the users namespace
+# Register the users namespace
 api.add_namespace(users_ns, path='/api/v1/users')
 api.add_namespace(amenities_ns, path='/api/v1/amenities')
 api.add_namespace(places_ns, path='/api/v1/places')
