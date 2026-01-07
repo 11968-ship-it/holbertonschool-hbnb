c67f3124b0c8f3514c623ad408c96a1eb9925e8d
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-07 19:47:50 +0300

    Update users.py

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -19,14 +19,20 @@ class UserList(Resource):
     def post(self):
         """Register a new user"""
         user_data = api.payload
-
-        # Simulate email uniqueness check (to be replaced by real validation with persistence)
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
+        try:
+            new_user = facade.create_user(user_data)
+            return {
+                'id': new_user.id,
+                'first_name': new_user.first_name,
+                'last_name': new_user.last_name,
+                'email': new_user.email
+            }, 201
+        except ValueError as e:
+            return {'error': str(e)}, 400
 
-        new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
     def get(self):
         """get User List"""
         users = facade.get_all_users()
