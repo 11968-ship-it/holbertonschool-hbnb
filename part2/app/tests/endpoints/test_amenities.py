d8117b23ce7da20dd10cdcfda1538ea393f256a3
11968-ship-it <11968@holbertonstudents.com>
2026-01-07 19:08:39 +0300

    Update test_amenities.py

--- part2/app/tests/endpoints/test_amenities.py
+++ part2/app/tests/endpoints/test_amenities.py
@@ -1,1 +1,19 @@
+import unittest
+from app import create_app
 
+class TestAmenityEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_amenity_success(self):
+        response = self.client.post('/api/v1/amenities/', json={"name": "Pool"})
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_amenity_failure(self):
+        response = self.client.post('/api/v1/amenities/', json={"name": ""})
+        self.assertEqual(response.status_code, 400)
+
+if __name__ == "__main__":
+    unittest.main()
