3bdfd24c2cf936e6061af87dd4d05fa2aa12602a
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-07 17:09:00 +0300

    Update review.py

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -24,10 +24,10 @@ class Review(BaseModel):
         self.user = user
 
     def to_dict(self):
-    return {
-        "id": self.id,
-        "text": self.text,
-        "rating": self.rating,
-        "user_id": self.user.id,
-        "place_id": self.place.id
-    }
+        return {
+            "id": self.id,
+            "text": self.text,
+            "rating": self.rating,
+            "user_id": self.user.id,
+            "place_id": self.place.id
+        }
