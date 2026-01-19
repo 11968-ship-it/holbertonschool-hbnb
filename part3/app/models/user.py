c16658106f91e5fbcdaac7bab0ef93caf6222293
Lamyaa439 <lamyaaalghaihab@gmail.com>
2026-01-19 18:17:31 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,5 +1,6 @@
 from app.models.base_model import BaseModel
 import re
+from app import bcrypt
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
@@ -9,6 +10,7 @@ class User(BaseModel):
         self.email = email
         self.is_admin = is_admin
         self.places = []
+        self.hash_password(password)
 
     # --- Getter & Setter for first_name ---
     @property
@@ -43,3 +45,11 @@ class User(BaseModel):
         if not re.match(email_regex, value):
             raise ValueError("Invalid email format")
         self._email = value
+   
+    def hash_password(self, password):
+    """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+    """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
