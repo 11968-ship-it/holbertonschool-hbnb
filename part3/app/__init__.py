ac057542034daca5237d782e6106d00e575b7c8d
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-19 17:52:56 +0300

    Add config_class parameter to create_app function
    Modify create_app function to accept a configuration class parameter.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from app.api import api
 
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+    
     api.init_app(app)
     return app
