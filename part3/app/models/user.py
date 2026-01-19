2fe074ddc651fee7d1f66c5cb97bab42736bd455
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-19 18:53:36 +0300

    Fix indentation for docstrings in user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -47,9 +47,9 @@ class User(BaseModel):
         self._email = value
    
     def hash_password(self, password):
-    """Hashes the password before storing it."""
+        """Hashes the password before storing it."""
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
-    """Verifies if the provided password matches the hashed password."""
+        """Verifies if the provided password matches the hashed password."""
         return bcrypt.check_password_hash(self.password, password)
