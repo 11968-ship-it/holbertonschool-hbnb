464c4635ae63cf44603c7eea08b728a9499a12ea
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-20 17:21:15 +0300

    Add auth namespace to API initialization

--- part3/app/api/__init__.py
+++ part3/app/api/__init__.py
@@ -4,6 +4,8 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.auth import api as auth_ns
+
 api = Api(
     version='1.0',
     title='HBnB API',
@@ -15,3 +17,4 @@ api.add_namespace(users_ns, path='/api/v1/users')
 api.add_namespace(amenities_ns, path='/api/v1/amenities')
 api.add_namespace(places_ns, path='/api/v1/places')
 api.add_namespace(reviews_ns, path='/api/v1/reviews')
+api.add_namespace(auth_ns, path="/auth")
