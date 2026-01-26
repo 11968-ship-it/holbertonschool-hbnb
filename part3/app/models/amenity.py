a76d11d376a6371c619a2774b35486698219d4c4
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-26 18:08:09 +0300

    Update amenity.py

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,18 +1,16 @@
 from app.models.base_model import BaseModel
-
+from app import db
+from sqlalchemy.orm import validates
 
 class Amenity(BaseModel):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
+    __tablename__ = 'amenities'
 
-    # --- Getter & setter for name ---
-    @property
-    def name(self):
-        return self._name
+    name = db.Column(db.String(50), nullable=False)
 
-    @name.setter
-    def name(self, value):
-        if not value or not isinstance(value, str) or len(value) > 50:
-            raise ValueError("Name is required and must be under 50 characters")
-        self._name = value
+    @validates("name")
+    def validate_name(self, key, value):
+        if not isinstance(value, str) or not value.strip():
+            raise ValueError("Name is required")
+        if len(value.strip()) > 50:
+                    raise ValueError("Name must be under 50 characters")
+        return value.strip()
