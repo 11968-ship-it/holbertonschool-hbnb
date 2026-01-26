3667e3cfddad2e0167ce20e6a3cff9c67e4a792a
11968-ship-it <11968@holbertonstudents.com>
2026-01-26 18:39:30 +0300

    Update facade.py

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -8,9 +8,9 @@ from app.persistence.repositories.user_repository import UserRepository
 class HBnBFacade:
     def __init__(self):
         self.user_repo = UserRepository()
-        self.place_repo = PlaceRepository()
-        self.review_repo = ReviewRepository()
-        self.amenity_repo = AmenityRepository()
+        self.place_repo =  SQLAlchemyRepository(Place)
+        self.review_repo =  SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
         
     
