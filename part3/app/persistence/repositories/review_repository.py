531f16e2739967e57f4b9fd36424de8054f4a552
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-26 18:24:27 +0300

    Update review_repository.py

--- part3/app/persistence/repositories/review_repository.py
+++ part3/app/persistence/repositories/review_repository.py
@@ -1,1 +1,10 @@
+from app.models.review import Review
+from app.persistence.repository import SQLAlchemyRepository
 
+
+class ReviewRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Review)
+
+    def get_by_rating(self, rating: int):
+        return self.model.query.filter_by(rating=rating).all()
