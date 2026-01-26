57e5b0e3b675f7c515ffc7435b2383f42f68c4b6
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-26 18:10:29 +0300

    Update place.py

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,13 +1,12 @@
 from app.models.base_model import BaseModel
 from app import db, bcrypt
-import uuid
 
 
 class Place(BaseModel):
-    __tablename__ = 'place'
+    __tablename__ = 'places'
     
     title = db.Column(db.String(100), nullable=False) #Title of the place.
-    description = db.Column(db.Text, nullable=False) # Description of the place.
+    description = db.Column(db.Text, nullable=True) # Description of the place.
     price = db.Column(db.Float, nullable=False) # Price per night.
     latitude = db.Column(db.Float, nullable=False) # Latitude of the place.
     longitude = db.Column(db.Float, nullable=False) # Longitude of the place.
