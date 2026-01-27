ad9dc7b5dc0b034ced0df0b0298710c0a9b64819
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-27 17:08:13 +0300

    Update repository.py

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -73,8 +73,8 @@ class SQLAlchemyRepository(Repository):
             db.session.commit()
             return True
         except Exception as e:
-        db.session.rollback()
-        raise e
+            db.session.rollback()
+            raise e
 
     def get_by_attribute(self, attr_name, attr_value):
         return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
