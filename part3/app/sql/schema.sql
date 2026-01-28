ae3f4054ee89165c09ca7f9b557da0e4ff1318a4
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-28 19:08:20 +0300

    Update schema.sql

--- part3/app/sql/schema.sql
+++ part3/app/sql/schema.sql
@@ -1,1 +1,23 @@
+-- ====================
+-- HBnB Schema (SQLite)
+-- ====================
+PRAGMA foreign_keys = ON;
 
+-- ---------- User --------------
+CREATE TABLE IF NOT EXISTS users (
+    id         CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    is_admin   BOOLEAN NOT NULL DEFAULT 0
+);
+
+
+-- ---------- Place --------------
+
+
+-- ---------- Review --------------
+
+
+-- ---------- Amenity --------------
