546db63038b4558dc55cc4c7c0c87244106cd782
11968-ship-it <11968@holbertonstudents.com>
2026-01-28 19:54:04 +0300

    Update schema.sql

--- part3/app/sql/schema.sql
+++ part3/app/sql/schema.sql
@@ -62,10 +62,6 @@ CREATE TABLE IF NOT EXISTS Amenity (
     name VARCHAR(255) UNIQUE NOT NULL
 );
 
-INSERT INTO "Amenity" (id, name) VALUES
-('f1b2c3d4-1111-4e2a-aaaa-1234567890ab', 'WiFi'),
-('f1b2c3d4-2222-4e2a-bbbb-1234567890ac', 'Swimming Pool'),
-('f1b2c3d4-3333-4e2a-cccc-1234567890ad', 'Air Conditioning');
 
 -- ---- Place_Amenity (Many-to-Many) ----------
 CREATE TABLE IF NOT EXISTS Place_Amenity (
