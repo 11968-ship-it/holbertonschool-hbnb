e74b4e5ffbf88ba5f9e37ae02530341e6bc9da67
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-01-28 18:30:04 +0300

    Update review_repository.py

--- part3/app/persistence/repositories/review_repository.py
+++ part3/app/persistence/repositories/review_repository.py
@@ -8,3 +8,10 @@ class ReviewRepository(SQLAlchemyRepository):
 
     def get_by_rating(self, rating: int):
         return self.model.query.filter_by(rating=rating).all()
+
+    def get_by_user_and_place(self, user_id, place_id):
+    """Find review by user and place"""
+    return self.model.query.filter_by(
+        user_id=user_id,
+        place_id=place_id
+    ).first()
