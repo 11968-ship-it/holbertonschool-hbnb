1c602389970b29e8a8639d4220c75841b381790d
Yara Alrasheed <11982@holbertonstudents.com>
2026-01-28 19:52:05 +0300

    Update seed.sql

--- part3/app/sql/seed.sql
+++ part3/app/sql/seed.sql
@@ -1,3 +1,19 @@
 -- ====================
 -- HBnB Seed Data
 -- ====================
+
+INSERT INTO User (
+    id,
+    email,
+    first_name,
+    last_name,
+    password,
+    is_admin
+) VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'admin@hbnb.io',
+    'Admin',
+    'HBnB',
+    '$2b$12$OBOKagu1u1QSSGzvvBeeoeRaAW4H/nLIIyrZehUnZauE1Dgx/Mnte',
+    1
+);
