c5ec4699a3cc70243416c412e82e894e194d6f76
Yara Alrasheed <11982@holbertonstudents.com>
2026-02-08 18:49:05 +0300

    Update scripts.js

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -1,11 +1,3 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
-document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
 document.addEventListener('DOMContentLoaded', () => {
     const token = getCookie('jwt'); // Assume you have a function to get JWT from cookie
     if (token) {
