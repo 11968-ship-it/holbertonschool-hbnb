bf732773c532ae80c5205872b4ccea0f68661a49
11968-ship-it <11968@holbertonstudents.com>
2026-02-08 17:58:08 +0300

    Update scripts.js

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -6,3 +6,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
   });
+document.addEventListener('DOMContentLoaded', () => {
+    const token = getCookie('jwt'); // Assume you have a function to get JWT from cookie
+    if (token) {
+        document.getElementById('add-review-section').style.display = 'block';
+    }
+});
