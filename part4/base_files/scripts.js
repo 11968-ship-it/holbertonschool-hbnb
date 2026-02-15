099176acc8a4e4f18d103f726b8efa538698094e
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-02-15 19:25:10 +0300

    Update scripts.js

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -43,7 +43,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (currentPage === 'index') {
     checkAuthentication();
     initPriceFilter();
-    setupLogout();
     populateLocationDatalist();
     setupIndexSearch();
     initCustomDatePicker(); 
